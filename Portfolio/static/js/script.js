43f3ff593f6b1c382fb884f78930d8873aa2acb1
Dipendra Pratap <58151564+dipdregan@users.noreply.github.com>
2023-03-20 23:09:37 -0700

    Add files via upload

---
+++ Portfolio/static/js/script.js
@@ -0,0 +1,18 @@
+/*================================= typing animation ===============================*/
+var typed = new Typed(".typing",{
+    strings:[" ","iNeuron Wala Londa","Data Scientist","Machine Learning Engineer","Web Developer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".text",{
+    strings:[" ","I'm a data scientist with extensive internship experience forover 2 years. My expertise is to cleaning data and crating a good model for best data prediction. I you want to say,- who i am ?.. you can say Machine Learning Engineer,Computer Vision,Natural Language Processing and many more....."],
+    typeSpeed:100,
+    BackSpeed:60,
+})
+var typed = new Typed(".name",{
+    strings:[" ","Dipendra Pratap Singh","and Nick is Dipak","Dreg_Warrior","Alone Walker","Leader","Dipendra Pratap"],
+    typeSpeed:100,
+    BackSpeed:60,
+    
+})
